e6d1d957c0fa1f393b787f0ee5fa9a43b61eba92
Chedly <chedly.marzougui@gmail.com>
2023-10-21 17:16:18 +0100

    Setup project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TodoList from './components/TodoList.vue'
+</script>
+
+<template>
+  <h1 class="text-4xl">Todo App</h1>
+  <TodoList/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+ <h2 class="text-2xl">Todo List</h2>
+</template>
+
+<style scoped>
+</style>
